607909bbacd253da4c0f81cc37ccb50f6a6ebf17
ImRS123 <152219283+ImRS123@users.noreply.github.com>
2023-12-03 02:25:48 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,31 +8,8 @@ st.title("My Dashboard")
 
 # Save the HTML file
 with open("dashboard.html", "w") as f:
-    f.write("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<style>
-body {
-  margin: 0;
-  padding: 0;
-}
-
-iframe {
-  width: 100vw;
-  height: 100vh;
-}
-</style>
-</head>
-<body>
-<iframe src="https://app.powerbi.com/view?r=eyJrIjoiNTcyNTcyNDItMjZiNS00ZmYzLWJkMTYtMzhiODM5OWY1Y2I1IiwidCI6IjJiZDAyMzJmLWNjZTEtNDZkMS04ZWE2LTQzMTE1NTVlMzAyYiJ9" 
-  frameborder="0" allowFullScreen="true"
-  height = "450px"
-  style = "width:100%; border:none;"></iframe>
-</body>
-</html>
-""")
+    f.read()
 
 # Open the HTML file in a web browser
-import webbrowser
-webbrowser.open("dashboard.html")
+#import webbrowser
+#webbrowser.open("dashboard.html")
